dbd6bd3d4f30a5a03f5b0acd8a942d40a1f9bdf7
Anderson Maicon Lemes De Souza Chaves <andersonchaves30@gmail.com>
2024-09-28 14:56:55 -0300

    feat: final adjustments, review and next steps

--- js/favorites.js
+++ js/favorites.js
@@ -1,16 +1,4 @@
-export class GithubUser {
-  static search(username) {
-    const endpoint = `https://api.github.com/users/${username}`;
-    return fetch(endpoint)
-      .then((data) => data.json())
-      .then(({ login, name, public_repos, followers }) => ({
-        login,
-        name,
-        public_repos,
-        followers,
-      }));
-  }
-}
+import { GithubUser } from './githubUser.js';
 // classe que vai conter a lógica dos dados
 // como os dados serão estruturados
 export class Favorites {
@@ -27,6 +15,12 @@ export class Favorites {
   async add(username) {
     try {
       const user = await GithubUser.search(username);
+      const userExists = this.entries.find(
+        (entry) => entry.login === user.login
+      );
+      if (userExists) {
+        throw new Error('Usuário já cadastrado!');
+      }
       if (user.login === undefined) {
         throw new Error('Usuário não encontrado!');
       }
@@ -38,7 +32,10 @@ export class Favorites {
     }
   }
   delete(user) {
-    this.entries = this.entries.filter((entry) => entry.login !== user.login);
+    const filteredEntries = this.entries.filter(
+      (entry) => entry.login !== user.login
+    );
+    this.entries = filteredEntries;
     this.update();
     this.save();
   }
@@ -66,18 +63,17 @@ export class FavoritesView extends Favorites {
         '.user img'
       ).src = `https://github.com/${user.login}.png`;
       row.querySelector('.user img').alt = `Imagem de ${user.name}`;
+      row.querySelector('.user a').href = `https://github.com/${user.login}`;
       row.querySelector('.user p').textContent = user.name;
       row.querySelector('.user span').textContent = user.login;
       row.querySelector('.repositories').textContent = user.public_repos;
       row.querySelector('.followers').textContent = user.followers;
-
       row.querySelector('.remove').onclick = () => {
         const isOK = confirm('Tem certeza que deseja deletar essa linha?');
         if ('isOk') {
           this.delete(user);
         }
       };
-
       this.tbody.append(row);
     });
   }
