c9fe7262d9f888fa9c09892dfc1867dc41c0e86d
Anderson Maicon Lemes De Souza Chaves <andersonchaves30@gmail.com>
2024-09-28 09:54:08 -0300

    feat: promises with async await

--- js/favorites.js
+++ js/favorites.js
@@ -11,25 +11,38 @@ export class GithubUser {
       }));
   }
 }
+// classe que vai conter a lógica dos dados
+// como os dados serão estruturados
 export class Favorites {
   constructor(root) {
     this.root = document.querySelector(root);
     this.load();
-    GithubUser.search('andersonchavesS').then(user => console.log(user))
   }
   load() {
     this.entries = JSON.parse(localStorage.getItem('@github-favorites:')) || [];
+  }
+  async add(username) {
+    const user = await GithubUser.search(username);
   }
   delete(user) {
     this.entries = this.entries.filter((entry) => entry.login !== user.login);
     this.update();
   }
 }
+// classe que vai criar a visualização e eventos do HTML
 export class FavoritesView extends Favorites {
   constructor(root) {
     super(root);
     this.tbody = this.root.querySelector('table tbody');
     this.update();
+    this.onadd();
+  }
+  onadd() {
+    const addButton = this.root.querySelector('.search button');
+    addButton.onclick = () => {
+      const { value } = this.root.querySelector('.search input');
+      this.add(value);
+    };
   }
   update() {
     this.removeAllTr();
